a5bb1265c29383a50c717d94b208031831786a4e
Thien Le <Thienlb15411@st.uel.edu.vn>
2019-12-12 19:27:01 +0700

    Check test defimal format

--- XuLyDuLieuSo/src/thienle/ifactory/TestDuLieuSo.java
+++ XuLyDuLieuSo/src/thienle/ifactory/TestDuLieuSo.java
@@ -1,11 +1,31 @@
 package thienle.ifactory;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class TestDuLieuSo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Test dữ liệu số");
+		DecimalFormat dcf=new DecimalFormat("#,###");
+
+		DecimalFormatSymbols dfs=
+
+		new DecimalFormatSymbols(Locale.getDefault());
+
+		dfs.setGroupingSeparator('.');
+
+		int x=124350;
+
+		dcf.setDecimalFormatSymbols(dfs);
+
+		System.out.println(dcf.format(x));
+		
+		
+		
+		
 			
 	}
 
